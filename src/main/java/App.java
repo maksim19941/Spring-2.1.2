9757700559dca00b322d388bd15276c5c013fc9f
MaxZagorskiy <mxm.zagorskij@gmail.com>
2024-03-18 22:16:49 +0300

    18.03.24

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloBean1 == helloBean2);
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1 == catBean2);
+
+
     }
+
+
+
+
+
+
+
+
+
+
 }
